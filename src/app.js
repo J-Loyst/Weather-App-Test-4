de8275116852aa6af5993bd54788969c2af395da
Julia Loyst <81717576+J-Loyst@users.noreply.github.com>
2021-05-24 15:02:10 -0400

    commit js changes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = response.data.main.temp;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
 
 let apiKey = `6fda14409b2eb3e68886c6b8cbdc1307`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Austin&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
