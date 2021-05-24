5265bdc4709537a3a8288323b2fb38e246a53382
Julia Loyst <81717576+J-Loyst@users.noreply.github.com>
2021-05-24 15:36:35 -0400

    js changes to temp functions

--- src/app.js
+++ src/app.js
@@ -3,9 +3,17 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let highTempElement = document.querySelector("#highTemp");
+  let lowTempElement = document.querySelector("#lowTemp");
+  windElement.innerHTML = Math.round(response.data.wind.speed) + " km/h";
+  humidityElement.innerHTML = response.data.main.humidity;
   descriptionElement.innerHTML = response.data.weather[0].description;
   cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  highTempElement.innerHTML = Math.round(response.data.main.temp_max);
+  lowTempElement.innerHTML = Math.round(response.data.main.temp_min);
 }
 
 let apiKey = `6fda14409b2eb3e68886c6b8cbdc1307`;
